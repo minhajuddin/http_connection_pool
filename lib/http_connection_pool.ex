ca7bc02a0119108d3136b8e179c167f0b8deac32
Khaja Minhajuddin <minhajuddink@gmail.com>
2019-03-08 00:15:47 -0500

    Printable

--- lib/http_connection_pool.ex
+++ lib/http_connection_pool.ex
@@ -7,16 +7,13 @@ defmodule HTP do
   if it comes from the database, an external API or others.
   """
 
-  @chars 0..255
-         |> Enum.filter(fn x -> String.printable?(<<x>>) end)
-         |> Enum.with_index()
-         |> Enum.map(fn {x, idx} -> {idx, x} end)
-         |> Enum.into(%{})
-  @chars_length Enum.count(@chars)
   def chars(size) do
     :crypto.strong_rand_bytes(size)
+    |> Base.encode64()
     |> :erlang.binary_to_list()
-    |> Enum.map(fn x -> @chars[rem(x, @chars_length)] end)
+    |> Enum.chunk_every(80)
+    |> Enum.intersperse(?\n)
     |> :erlang.iolist_to_binary()
+    |> :binary.part(0, size)
   end
 end
