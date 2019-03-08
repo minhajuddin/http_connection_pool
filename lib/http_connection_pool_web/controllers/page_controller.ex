b201e3c59194c095616e644fde8e5ac43e8a9dd0
Khaja Minhajuddin <minhajuddink@gmail.com>
2019-03-08 09:39:18 -0500

    1kb

--- lib/http_connection_pool_web/controllers/page_controller.ex
+++ lib/http_connection_pool_web/controllers/page_controller.ex
@@ -10,11 +10,13 @@ defmodule HTPWeb.PageController do
     |> send_resp(200, body_for(params["size"]))
   end
 
+  @onekb HTP.chars(1 * 1024)
   @tenkb HTP.chars(10 * 1024)
   @hundredkb HTP.chars(100 * 1024)
   @thousandkb HTP.chars(1000 * 1024)
   # returns a body of size KB
   defp body_for(size)
+  defp body_for("1kb"), do: @onekb
   defp body_for("10kb"), do: @tenkb
   defp body_for("100kb"), do: @hundredkb
   defp body_for("1000kb"), do: @thousandkb
